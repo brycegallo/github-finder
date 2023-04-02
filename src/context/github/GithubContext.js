74c26c0068122c968ad44b248a0ecaab1b1df393
bryce <76966016+brycegallo@users.noreply.github.com>
2023-04-02 19:14:43 -0400

    Link User Page link user profile to user page component

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,35 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 export const GithubProvider = ({ children }) => {
     const initialState = {
         users: [],
+        user: {},
         loading: false
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    // get initial users (for testing purposes)
+    // get a single user
+    const getUser = async (login) => {
+        setLoading()
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+            headers: {
+                Authorization: `Bearer ${GITHUB_TOKEN}`,
+            },
+        })
+
+        if(response.status === 404) {
+            window.location = '/notfound'
+        } else {
+            const data = await response.json()
+
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+    }
+
+    // get users
     const searchUsers = async (text) => {
         setLoading()
 
@@ -35,6 +58,7 @@ export const GithubProvider = ({ children }) => {
             payload: items,
         })
     }
+
     // Clear users
     const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
 
@@ -43,9 +67,11 @@ export const GithubProvider = ({ children }) => {
 
     return <GithubContext.Provider value={{
         users: state.users,
+        user: state.user,
         loading: state.loading,
         searchUsers,
         clearUsers,
+        getUser,
     }}>
         {children}
     </GithubContext.Provider>
