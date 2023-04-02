02d5abb71bef3491ba4cb2e0cababe7a01057601
bryce <76966016+brycegallo@users.noreply.github.com>
2023-04-01 21:07:31 -0400

    Create Alert from Context create and display alert using context

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -35,6 +35,8 @@ export const GithubProvider = ({ children }) => {
             payload: items,
         })
     }
+    // Clear users
+    const clearUsers = () => dispatch({ type: 'CLEAR_USERS' })
 
     // Set loading
     const setLoading = () => dispatch({ type: 'SET_LOADING' })
@@ -43,6 +45,7 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         searchUsers,
+        clearUsers,
     }}>
         {children}
     </GithubContext.Provider>
